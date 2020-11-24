bc4f4236d1b99d9fbc95fb8f99a057c28288cc23
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2020-11-24 16:44:32 +0000

    Few classes created and Glossario deleted

---
+++ 4Source/4Source/Terreno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Source
+{
+    class Terreno
+    {
+        int id;
+        string indiceCont;
+        double area;
+        double imi;
+    }
+}
