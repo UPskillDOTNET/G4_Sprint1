536a748d0d11ebbc811872ad30d87e81a093a736
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2020-11-25 22:54:11 +0000

    Class Funcionario changed

--- 4Source/4Source/model/Funcionario.cs
+++ 4Source/4Source/model/Funcionario.cs
@@ -8,8 +8,18 @@ namespace _4Source
 {
     class Funcionario : Pessoa
     {
-        string cargo { get; set; }
-        string numeroFunc { get; set; }
+        private string cargo;
+        private string numeroFunc;
+
+        public string Cargo {
+            get { return cargo; }
+            set { cargo = value; }
+        }
+
+        public string NumeroFunc {
+            get { return numeroFunc; }
+            set { numeroFunc = value; }
+        }
 
         public Funcionario(string cargo, string numeroFunc, string nome, string nif, DateTime dataNascimento) : base(nome, nif, dataNascimento)
 
