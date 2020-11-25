a965e507d6e630cfb2d688aa5aa5082b94ca0fca
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2020-11-25 14:40:58 +0000

    Update class calssificacao

--- 4Source/4Source/model/Classificacao.cs
+++ 4Source/4Source/model/Classificacao.cs
@@ -4,25 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _4Source
-{
-    class Classificacao
-    {
-        string desc { get; set; }
-        double areaConst { get; set; }
-        DateTime dataConst { get; set; }
-
-        public Classificacao(string desc, double areaConst, DateTime dataConst) 
-        {
-            this.desc = desc;
-            this.areaConst = areaConst;
-            this.dataConst = dataConst;
-        }
-
-        public Classificacao() {
-            this.desc = "Lorem ipsilum";
-            this.areaConst = 5000;
-            this.dataConst = new DateTime(1970, 1, 1);
-        }
+namespace _4Source {
+    class Classificacao {
+        string desc;
+        double areaConst;
+        string dataConst;
     }
 }
