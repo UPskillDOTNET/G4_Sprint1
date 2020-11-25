039cd33936afe57faac3d5a5bc7c578bc6fe7877
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2020-11-25 09:26:38 +0000

    MVC structure added

---
+++ 4Source/4Source/model/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Source
+{
+    class Pessoa
+    {
+        string nome;
+        string nif;
+        DateTime dataNascimento;
+
+        public Pessoa(string nome, string nif, DateTime dataNascimento)
+        {
+            this.nome = nome;
+            this.nif = nif;
+            this.dataNascimento = dataNascimento;
+        }
+
+        public Pessoa()
+        {
+            this.nome = "John Wick";
+            this.nif = "999999999";
+            this.dataNascimento = new DateTime(1970, 1, 1);
+        }
+    }
+}
