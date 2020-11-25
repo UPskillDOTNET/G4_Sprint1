fb9d05296a4f91e81ba2f441c41251ecf19d8368
caio_reis <caiocruzeiror@upskill.pt>
2020-11-25 12:00:08 +0000

    Alterações no Funcionario

--- 4Source/4Source/model/Funcionario.cs
+++ 4Source/4Source/model/Funcionario.cs
@@ -14,7 +14,9 @@ namespace _4Source
         public Funcionario(string cargo, string numeroFunc, string nome, string nif, DateTime dataNascimento) : base(nome, nif, dataNascimento)
 
         {
-            
+            this.cargo = cargo;
+            this.numeroFunc = numeroFunc;
+
         }
     }   
 }
