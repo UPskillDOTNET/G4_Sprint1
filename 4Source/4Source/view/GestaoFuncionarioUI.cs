c8ca41d0a2a58f5ff661be6b23af0fc4db5be5ed
Tiago Azevedo <tiagomina.azevedo@upskill.pt>
2020-11-26 10:22:25 +0000

    Changes to UI

---
+++ 4Source/4Source/view/GestaoFuncionarioUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Source.views
+{
+    class GestaoFuncionarioUI
+    {
+        public static void MainFuncionario()
+        {
+
+        }
+
+        public static void Menu()
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    //criarPessoa();
+                    break;
+                case "2":
+                    //editarPessoa();
+                    break;
+                case "3":
+                    //listarPessoa();
+                    break;
+                case "4":
+                    //eliminarPessoa();
+                    break;
+                case "5":
+                    //voltarMenuPrincipal();
+                    break;
+                default:
+                    Console.WriteLine("Wrong option");
+                    break;
+            }
+        }
+    }
+}
