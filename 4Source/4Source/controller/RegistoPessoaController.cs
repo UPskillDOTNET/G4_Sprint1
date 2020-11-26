d93ea81c4644ae950704ff49c6c741beec0b18d3
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2020-11-26 22:51:35 +0000

    Registo Pessoa Controller Updated

--- 4Source/4Source/controller/RegistoPessoaController.cs
+++ 4Source/4Source/controller/RegistoPessoaController.cs
@@ -1,16 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
 using _4Source;
+using _4Source.persistencia;
 
 namespace _4Source.controllers
 {
-    class RegistoPessoaController
+    public class RegistoPessoaController
     {
-        
-    }
+        public static bool RegistarPessoa(Pessoa pessoa)
+        {
+            bool flag = true;
+            try
+            {
+                Autarquia autarquia = Dados.CarregarDados();
+                autarquia.RegistarPessoa(pessoa);
+                Dados.GuardarDados(autarquia);
+            }
+            catch (NifDuplicadoException e)
+            {
+                flag = false;
+                Console.WriteLine("Advertencia: " + e.ToString());
+            }
+            return flag;
+        }
 
+        public static Pessoa PesquisarPessoa(string nif)
+        {
+            Pessoa pessoa = null;
+            Autarquia autarquia = Dados.CarregarDados();
+            pessoa = autarquia.PesquisarPessoa(nif);
+            return pessoa;
+        }
+
+        public static bool AlterarPessoa(Pessoa pessoa)
+        {
+            bool flag = true;
+            try
+            {
+                Autarquia autarquia = Dados.CarregarDados();
+                autarquia.AlterarPessoa(pessoa);
+                Dados.GuardarDados(autarquia);
+            }
+            catch (ElementoNaoExistenteException e)
+            {
+                flag = false;
+                Console.WriteLine("Advertencia: " + e.ToString());
+            }
+            return flag;
+        }
+
+        public static Pessoa EliminarPessoa(string nif)
+        {
+
+            Pessoa pessoa = null;
+            try
+            {
+                Autarquia autarquia = Dados.CarregarDados();
+                pessoa = autarquia.EliminarPessoa(nif);
+                Dados.GuardarDados(autarquia);
+            }
+            catch (ElementoNaoExistenteException e)
+            {
+
+                Console.WriteLine("Advertencia: " + e.ToString());
+            }
+            return pessoa;
+        }
+    }
 }
