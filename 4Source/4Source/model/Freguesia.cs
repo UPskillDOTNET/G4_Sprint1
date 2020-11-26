3264265ea71e335301c595b91cc97b4b86d2a3d3
Vaazch <joaom.vodafone@upskill.pt>
2020-11-26 10:06:45 +0000

    Update Freguesia.cs
    revert

---
+++ 4Source/4Source/model/Freguesia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _4Source
+{
+    public class Freguesia
+    {
+        string nome { get; set; }
+        ArrayList terrenoList;
+
+        public Freguesia(string nome)
+        {
+            this.nome = nome;
+            terrenoList = new ArrayList();
+        }
+
+        //public void AddToList(Terreno.id)
+        //{
+            //terrenoList.Add(Terreno.id);
+        //}
+    }
+}
