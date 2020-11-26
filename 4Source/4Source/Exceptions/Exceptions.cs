e863beb38831ffdf8fcfd59df3b2202d4c6232e5
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2020-11-26 11:00:29 +0000

    Exceptions file created

---
+++ 4Source/4Source/Exceptions/Exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _4Source.Exceptions {
+    public class DataInvalidaException : Exception {
+        public DataInvalidaException(string message) : base(message) {
+        }
+    }
+    public class NifInvalidoException : Exception {
+        public NifInvalidoException(string message) : base(message) {
+        }
+    }
+    public class NomeInvalidoException : Exception {
+        public NomeInvalidoException(string message) : base(message) {
+        }
+    }
+    public class NumeroFuncionarioInvalidoException : Exception {
+        public NumeroFuncionarioInvalidoException(string message) : base(message) {
+        }
+    }
+
+
+    public class NifDuplicadoException : Exception {
+        public NifDuplicadoException(string message) : base(message) {
+        }
+    }
+    public class NumeroFuncionarioDuplicadoException : Exception {
+        public NumeroFuncionarioDuplicadoException(string message) : base(message) {
+        }
+    }
+    public class ElementoNaoExistenteException : Exception {
+        public ElementoNaoExistenteException(string message) : base(message) {
+        }
+    }
+
+    public class NomePessoaInvalidoException : Exception {
+        public NomePessoaInvalidoException(string message) : base(message) {
+        }
+    }
+
+
+}
