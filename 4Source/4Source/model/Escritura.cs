5a49fec3aa61d622005bf0002d5d6361ce52c23e
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2020-11-27 00:34:10 +0000

    Escritura model changed

--- 4Source/4Source/model/Escritura.cs
+++ 4Source/4Source/model/Escritura.cs
@@ -4,20 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _4Source
-{
-    class Escritura : Terreno
-    {
-        
-        int num { get; set;  }
-        DateTime data { get; set; }
+namespace _4Source {
+    class Escritura : Terreno {
 
-        public Escritura(int id, double indiceCont, string forma, double area, double imi, int num, DateTime data) : base(id, indiceCont, forma, area, imi)
-        {
-        
-        this.num = num;
-        this.data = data;
-        
+        int num;
+        DateTime data;
+
+        public Escritura(int id, double indiceCont, string forma, double area, double imi, int num, DateTime data) : base(id, indiceCont, forma, area, imi) {
+
+            this.num = num;
+            this.data = data;
+
         }
-    }   
+        public int Num   // property
+        {
+            get { return num; }   // get method
+            set { num = value; }
+        }
+
+        public DateTime Data {
+            get { return data; }
+            set { data = value; }
+        }
+
+    }
 }
