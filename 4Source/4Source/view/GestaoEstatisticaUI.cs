aa6c37864465b1d4d0b4e5bd62cf4fb96c49b821
TiagoAzevedoUpSkill <tiagomina.azevedo@upskill.pt>
2020-11-30 12:42:23 +0000

    Update GestaoEstatisticaUI.cs
    Menu criado

--- 4Source/4Source/view/GestaoEstatisticaUI.cs
+++ 4Source/4Source/view/GestaoEstatisticaUI.cs
@@ -6,55 +6,84 @@ using System.Threading.Tasks;
 
 namespace _4Source.views
 {
-    //class GestaoEstatisticaUI
-    //{
-    //    public static void MainEstatistica()
-    //    {
+    class GestaoEstatisticaUI
+    {
+        public static void MainEstatistica()
+        {
 
-    ////       int numInput;
-    //            Console.Clear();
-    //            Console.ForegroundColor = ConsoleColor.Yellow;
-    //            Console.WriteLine("\n\nBem vindo. Escolha a opção pretendida\n");
-    //            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
-    //            Console.ResetColor();
-    //            Console.WriteLine("1) Gestão de Pessoas");
-    //            Console.WriteLine("2) Gestão de Funcionários");
-    //            Console.WriteLine("3) Gestão de Freguesias");
-    //            Console.WriteLine("4) Gestão de Terrenos");
-    //            Console.WriteLine("5) Gestão de Escritura");
-    //            Console.WriteLine("6) Cálculo de Estatisticas");
-    //            Console.WriteLine("\n7) Fechar Programa\n\n");
-    //        do
-    //        {
-    //            switch (numInput)
-    //            {
-    //                case 0:
-    //                    Console.WriteLine("Volta para o menu anterior.");
-    //                    break;
-    //                case 1:
-    //                    RegistarFuncionario();
-    //                    break;
-    //                case 2:
-    //                    PesquisarFuncionario();
-    //                    break;
-    //                case 3:
-    //                    EliminarFuncionario();
-    //                    break;
-    //                case 4:
-    //                    AlterarFuncionario();
-    //                    break;
-    //                case 5:
-    //                    ListarFuncionarios();
-    //                    break;
-    //                    _4Source.views.Menu.mainMenu();
-    //                default:
-    //                    Console.WriteLine("Opção Errada");
-    //                    break;
-    //            }
+            int numInput;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n===Mostrar Estatisticas===");
+            Console.ResetColor();
+            Console.WriteLine("1) Apresentar lista de pessoas que nasceram antes duma determinada data.");
+            Console.WriteLine("2) Apresentar lista das 5 pessoas mais velhas.");
+            Console.WriteLine("3) Apresentar a área total de terrenos do concelho.");
+            Console.WriteLine("4) Apresentar a percentagem de área rural (terrenos rurais) do concelho.");
+            Console.WriteLine("5) Apresentar uma listagem das freguesias ordenadas por valor patrimonial dos terrenos do tipo urbano.");
+            Console.WriteLine("6) Apresentar uma listagem das freguesias ordenadas por dimensão.");
+            Console.WriteLine("7)  Apresentar a atividade rural predominante numa dada freguesia.");
+            Console.WriteLine("8)  Apresentar uma listagem dos terrenos do tipo industrial com a data de inspeção posterior a uma dada data.");
+            Console.WriteLine("9)  Consultar o top 5 das pessoas com mais terrenos.");
+            Console.WriteLine("\n10) Fechar Programa\n\n");
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                numInput = Utils.GetIntNumber("Por favor escolha uma opção:");
+                Console.ResetColor();
+                switch (numInput)
+                {
+                    case 0:
+                        Console.WriteLine("Volta para o menu anterior.");
+                        break;
+                    case 1:
+                        MostrarPessoasDeterminadaData();
+                        break;
+                    case 2:
+                        MostrarTop5PessoasMaisVelhas();
+                        break;
+                    case 3:
+                        MostrarAreaTotalAutarquia();
+                        break;
+                    case 4:
+                        MostrarPercentagemAreaRuralAutarquia();
+                        break;
+                    case 5:
+                        MostrarListaFreguesiasValorPatrimonial();
+                        break;
+                    case 6:
+                        MostrarFreguesiasDimensao();
+                        break;
+                    case 7:
+                        MostrarAreaPredominanteFreguesia();
+                        break;
+                    case 8:
+                        MostrarListaTerrenosInspecao();
+                        break;
+                    case 9:
+                        MostrarTop5PessoasMaisTerrenos();
+                        break;
+                    case 10:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("\nVolta para o menu anterior.");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Opção Errada");
+                        break;
+                }
 
-    //        } while (numInput != 0);
-    //    }
-
-    //}
+            } while (numInput != 0);
+        }
+        private static void MostrarPessoasDeterminadaData() { }
+        private static void MostrarTop5PessoasMaisVelhas() { }
+        private static void MostrarAreaTotalAutarquia() { }
+        private static void MostrarPercentagemAreaRuralAutarquia() { }
+        private static void MostrarListaFreguesiasValorPatrimonial() { }
+        private static void MostrarFreguesiasDimensao() { }
+        private static void MostrarAreaPredominanteFreguesia() { }
+        private static void MostrarListaTerrenosInspecao() { }
+        private static void MostrarTop5PessoasMaisTerrenos() { }
+    }
 }
 
